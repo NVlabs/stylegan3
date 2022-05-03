0bf00e127885bc457b17b1073288df503a6d896c
Diego Porres <daporres@ufm.edu>
2022-05-03 15:51:39 +0200

    Main branch commits for using VS2022 and using PyTorch 1.11

--- torch_utils/ops/bias_act.py
+++ torch_utils/ops/bias_act.py
@@ -43,7 +43,7 @@ def _init():
             sources=['bias_act.cpp', 'bias_act.cu'],
             headers=['bias_act.h'],
             source_dir=os.path.dirname(__file__),
-            extra_cuda_cflags=['--use_fast_math'],
+            extra_cuda_cflags=['--use_fast_math', '--allow-unsupported-compiler'],
         )
     return True
 
