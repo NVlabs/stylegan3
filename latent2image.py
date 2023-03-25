700bac1cb7b62a7d931445ab41dafa2e5e84e5e1
Julián Wallis <59374763+julianwallism@users.noreply.github.com>
2023-03-25 19:31:42 +0100

    Fresh Start
    Starting in ubuntu

--- latent2image.py
+++ latent2image.py
@@ -11,7 +11,6 @@
 import os
 import re
 from typing import List, Optional, Tuple, Union
-from time import perf_counter
 
 import click
 import dnnlib
@@ -71,29 +70,22 @@ def make_transform(translate: Tuple[float,float], angle: float):
 
 @click.command()
 @click.option('--network', 'network_pkl', help='Network pickle filename', default="ffhq1024.pkl", required=False)
-@click.option('--seeds', type=parse_range, help='List of random seeds (e.g., \'0,1,4-6\')', default=0, required=False)
-@click.option('--w_space', is_flag=True, help='Interpolate in W space instead of Z space')
 @click.option('--trunc', 'truncation_psi', type=float, help='Truncation psi', default=1, show_default=True)
 @click.option('--class', 'class_idx', type=int, help='Class label (unconditional if not specified)')
 @click.option('--noise-mode', help='Noise mode', type=click.Choice(['const', 'random', 'none']), default='const', show_default=True)
 @click.option('--translate', help='Translate XY-coordinate (e.g. \'0.3,1\')', type=parse_vec2, default='0,0', show_default=True, metavar='VEC2')
 @click.option('--rotate', help='Rotation angle in degrees', type=float, default=0, show_default=True, metavar='ANGLE')
-@click.option('--outdir_img', help='Where to save the output images', type=str, default="out/images", required=False, metavar='DIR')
-@click.option('--outdir_seeds', help='Where to save the latent vector', type=str, default="out/seeds", required=False, metavar='DIR')
-@click.option('--append_latent', is_flag=True, help='If true, append the latent vector to the latent vector file (if it exists). Otherwise, overwrite the file.')
+@click.option('--outdir_img', help='Where to save the output images', type=str, default="out/projected_images", required=False, metavar='DIR')
+@click.option('--latent_vector', help='Where to find the latent vector', type=str, required=True, metavar='DIR')
 def generate_images(
     network_pkl: str,
-    seeds: List[int],
     truncation_psi: float,
     noise_mode: str,
     outdir_img: str,
-    outdir_seeds: str,
     translate: Tuple[float,float],
     rotate: float,
     class_idx: Optional[int],
-    w_space: bool, 
     latent_vector: str,
-    append_latent: bool
 ):
     """Generate images using pretrained network pickle.
 
@@ -109,14 +101,7 @@ def generate_images(
         print("Loading Generator...")
         G = legacy.load_network_pkl(f)['G_ema'].to(device) # type: ignore
 
-    # Might use discriminator later to determine how fake the image is
-
-    # with dnnlib.util.open_url(network_pkl) as f:
-    #     print("Loading discriminator...")
-    #     D = legacy.load_network_pkl(f)['D'].to(device)
-
     os.makedirs(outdir_img, exist_ok=True)
-    os.makedirs(outdir_seeds, exist_ok=True)
 
     # Labels.
     label = torch.zeros([1, G.c_dim], device=device)
@@ -128,21 +113,13 @@ def generate_images(
         if class_idx is not None:
             print ('warn: --class=lbl ignored when running on an unconditional network')
 
-    vector_seeds = []
-    start_time = perf_counter()
+    # Open latent vector file (npy) and store in a list.
+    
+    seeds = np.load(latent_vector)
+
     # Generate images.
-    for seed_idx, seed in enumerate(seeds):
-        time_remaining = (perf_counter() - start_time) * (len(seeds) - seed_idx) / (seed_idx + 1)
-        print('Generating image for seed %d (%d/%d) ... Time Remaining: %d' % (seed, seed_idx, len(seeds), time_remaining))
-
-        z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
-
-        if w_space:
-            w = G.mapping(z, label)
-            vector_seeds.append(w.cpu().numpy())
-        else:
-            vector_seeds.append(z.cpu().numpy())
-
+    for idx, z in enumerate(seeds):
+        print(z)
         if hasattr(G.synthesis, 'input'):
             m = make_transform(translate, rotate)
             m = np.linalg.inv(m)
@@ -151,17 +128,8 @@ def generate_images(
         img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
         img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
 
-        PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir_img}/seed{seed:04d}.png')
+        PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir_img}/{idx:04d}.png')
     
-    vector_seeds = np.concatenate(vector_seeds, axis=0)
-    if(append_latent):
-        print("Appending latent vector to file...")
-        if(os.path.exists(f'{outdir_seeds}/seeds.npy')):
-            old_vector_seeds = np.load(f'{outdir_seeds}/seeds.npy')
-            vector_seeds = np.concatenate((old_vector_seeds, vector_seeds), axis=0)
-    np.save(f'{outdir_seeds}/seeds.npy', vector_seeds)
-
-
 
 #----------------------------------------------------------------------------
 
